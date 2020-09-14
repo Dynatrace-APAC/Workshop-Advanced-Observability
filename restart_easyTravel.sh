72a69272e4ffcdc7d165462ad8deea99e0e47bd4
Brandon Neo <brandon.neo@gmail.com>
2020-09-14 13:12:29 +1000

    Delete restart_easyTravel.sh

--- restart_easyTravel.sh
+++
@@ -1,6 +0,0 @@
-#! bin/bash
-
-killall /home/ubuntu/easyTravel/easytravel-2.0.0-x64/apache2.4/Linux_x64/bin/httpd
-killall /home/ubuntu/easyTravel/easytravel-2.0.0-x64/jre/bin/java
-killall /home/ubuntu/easyTravel/easytravel-2.0.0-x64/chrome32/chrome
-killall /home/ubuntu/easyTravel/easytravel-2.0.0-x64/chrome32/driver/chromedriver_linux64
